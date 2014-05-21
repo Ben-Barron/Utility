f8697518af024e58c73be80d17c2dd74db0a10e8
Ben Barron <forums@benbarron.com>
2014-05-21 22:19:49 +0100

    Observable FileSystemWatcher interface

---
+++ Utility.Reactive/IO/IObservableFileSystemWatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Utility.Reactive.IO
+{
+    public interface IObservableFileSystemWatcher
+    {
+        IObservable<FileSystemEventArgs> Changed { get; }
+        IObservable<RenamedEventArgs> Renamed { get; }
+        IObservable<FileSystemEventArgs> Deleted { get; }
+        IObservable<ErrorEventArgs> Errors { get; }
+        IObservable<FileSystemEventArgs> Created { get; }
+
+        void Start();
+        void Stop();
+    }
+}
