7d40fef5e80f77435334c00690d7e1f0e12e3f68
Ben Barron <forums@benbarron.com>
2014-06-08 12:29:12 +0100

    Added remove KeyValuePair extension method.

--- Utility/Extensions/ConcurrentDictionaryExtensions.cs
+++ Utility/Extensions/ConcurrentDictionaryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Utility.Extensions
 {
@@ -8,6 +9,15 @@ namespace Utility.Extensions
         {
             TValue value;
             return dictionary.TryRemove(key, out value);
+        }
+
+        /// <remarks>
+        /// This is a hack but is atomic at time of writing! Unlikely to change but a unit test exists to test it as best it can (not 100%!)
+        /// </remarks>
+        public static bool TryRemove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            ICollection<KeyValuePair<TKey, TValue>> collection = dictionary;
+            return collection.Remove(new KeyValuePair<TKey, TValue>(key, value));
         }
     }
 }
