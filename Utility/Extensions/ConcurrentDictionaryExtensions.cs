5c8511e3c01e3a7183a00ad1d04db4ea9e122bac
Ben Barron <forums@benbarron.com>
2014-06-08 19:57:33 +0100

    Change try remove key pair method semantics

--- Utility/Extensions/ConcurrentDictionaryExtensions.cs
+++ Utility/Extensions/ConcurrentDictionaryExtensions.cs
@@ -12,11 +12,10 @@ namespace Utility.Extensions
         }
 
         /// <remarks>
-        /// This is a hack but is atomic at time of writing! Unlikely to change but a unit test exists to test it as best it can (not 100%!)
+        /// HACK: is atomic at time of writing! Unlikely to change but a unit test exists to test it as best it can.
         /// </remarks>
-        public static bool TryRemove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        public static bool TryRemove<TKey, TValue>(this ICollection<KeyValuePair<TKey, TValue>> collection, TKey key, TValue value)
         {
-            ICollection<KeyValuePair<TKey, TValue>> collection = dictionary;
             return collection.Remove(new KeyValuePair<TKey, TValue>(key, value));
         }
     }
